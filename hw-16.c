2c3f3c2f3d2ae8db18865cf5024c15e20239ea4e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-16 14:52:10 -0500

    parent process and fork handling

--- hw-16.c
+++ hw-16.c
@@ -4,11 +4,30 @@
 #include <stdlib.h>
 
 void child(){
-    printf("PID: %d\n", getpid());
+    printf("Child PID: %d\n", getpid());
     sleep(rand() % 4 + 2);
-    printf("PID: %d\n finished", getpid());
+    printf("Child PID: %d finished\n", getpid());
+}
+
+void parent(){
+    printf("Parent to fork off PID: %d\n", getpid());
+
 }
 
 int main(){
     srand(time(NULL));
+    printf("Forking PID: %d\n", getpid());
+    int fork1 = fork();
+    if (fork1){
+        int fork2 = fork();
+        if (fork2){
+            parent();
+        }
+        else
+        {
+            child();
+        }
+    }else{
+        child();
+    }
 }
